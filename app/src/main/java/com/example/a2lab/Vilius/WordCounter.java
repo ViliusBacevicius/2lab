9483afbd5fd85ac39b39c4becce2dd3828d45e12
ViliusBacevicius <127504186+ViliusBacevicius@users.noreply.github.com>
2023-04-08 13:21:23 +0300

    Testavimu darbu užbaigimas

--- app/src/main/java/com/example/a2lab/Vilius/WordCounter.java
+++ app/src/main/java/com/example/a2lab/Vilius/WordCounter.java
@@ -3,10 +3,12 @@ package com.example.a2lab.Vilius;
 
 
 public class WordCounter {
-    public static String getWordsCount(String inputString) throws Exception {
+    public static String getWordsCount(String inputString) {
+        if (inputString != null) {
         String text = inputString.replace("\n", " ");
         String[] textArray = text.split(" ");
-        throw new Exception("NotImplemented!!!");
         return String.valueOf(textArray.length);
+        }
+        return "0";
     }
 }
--- app/src/test/java/com/example/a2lab/TextCounterUnitTest.java
+++ app/src/test/java/com/example/a2lab/TextCounterUnitTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 import com.example.a2lab.Vilius.TextCounter;
+import com.example.a2lab.Vilius.WordCounter;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -20,10 +21,11 @@ public class TextCounterUnitTest {
         String actual = TextCounter.getCharsCount(givenValue);
         assertEquals(expected, actual);
     }
+
     @Test
     public void getCharsCount_WhenNullString_ResultZero() {
 
-        String givenValue = null ;
+        String givenValue = null;
         String expected = "0";
         String actual = TextCounter.getCharsCount(givenValue);
         assertEquals(expected, actual);
@@ -32,9 +34,44 @@ public class TextCounterUnitTest {
     @Test
     public void getCharsCount_WhenFiveCharsString_ResultFive() {
 
-        String givenValue = "12345" ;
+        String givenValue = "12345";
         String expected = "5";
         String actual = TextCounter.getCharsCount(givenValue);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getWordsCount_WhenNullString_ResultZero() {
+
+        String givenValue = null;
+        String expected = "0";
+        String actual = WordCounter.getWordsCount(givenValue);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void getWordsCount_WhenSixWordsString_ResultSix() {
+
+        String givenValue = "one two three four five six";
+        String expected = "6";
+        String actual = WordCounter.getWordsCount(givenValue);
+        assertEquals(expected, actual);
+}
+
+    @Test
+    public void getWordsCount_WhenSpacesString_Resultzero() {
+
+        String givenValue = "                          ";
+        String expected = "0";
+        String actual = WordCounter.getWordsCount(givenValue);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getWordsCount_WhenSymbolsString_Resultone() {
+
+        String givenValue = "!@@!$#@%#$^$%&^%*(&^(^*&%E";
+        String expected = "1";
+        String actual = WordCounter.getWordsCount(givenValue);
         assertEquals(expected, actual);
     }
 }
